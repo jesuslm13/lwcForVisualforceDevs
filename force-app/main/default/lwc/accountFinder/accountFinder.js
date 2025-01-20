5d3ff5abfccf72ac5c4fe3683deb7f4cc6214b1a
SeungRyong Oh <jesuslm13@naver.com>
2025-01-20 23:50:16 +0900

    250120_Lightning Web Components for Visualforce Developers_Work with Salesforce Data

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -5,8 +5,8 @@
  * @last modified on  : 01-20-2025
  * @last modified by  : sr.Oh
 **/
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 export default class AccountFinder extends LightningElement {
     annualRevenue = null;
 
@@ -17,4 +17,7 @@ export default class AccountFinder extends LightningElement {
     reset(){
         this.annualRevenue = null;
     }
+
+    @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' })
+    accounts;
 }
--- force-app/main/default/lwc/accountSearch/accountSearch.js
+++ force-app/main/default/lwc/accountSearch/accountSearch.js
@@ -5,13 +5,18 @@
  * @last modified on  : 01-20-2025
  * @last modified by  : sr.Oh
 **/
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByEmployeeNumber from '@salesforce/apex/AccountListControllerLwc.queryAccountsByEmployeeNumber';
 export default class AccountSearch extends LightningElement {
     numberOfEmployees = null;
     handleChange(event) {
         this.numberOfEmployees = event.detail.value;
     }
+    
     reset() {
         this.numberOfEmployees = null;
     }
+
+    @wire(queryAccountsByEmployeeNumber, { numberOfEmployees: '$numberOfEmployees' })
+    accounts;
 }
