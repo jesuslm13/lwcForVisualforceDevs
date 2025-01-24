e4b38083bd2c7fd0a1863c88aec02b73676de72e
SeungRyong Oh <jesuslm13@naver.com>
2025-01-24 14:47:56 +0900

    250124_Lightning Web Components and Salesforce Data_Handle Server Errors

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,12 @@
+/**
+ * @description       : 
+ * @author            : sr.Oh
+ * @group             : 
+ * @last modified on  : 01-24-2025
+ * @last modified by  : sr.Oh
+**/
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
@@ -13,4 +21,8 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.contacts.error) ? reduceErrors(this.contacts.error) : [];
+    }
 }
